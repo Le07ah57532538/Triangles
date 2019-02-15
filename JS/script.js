1411a5d698a89119f120e9dc2d02ff5257346228
Leah Gakii <leahgakii@gmail.com>
2019-02-15 17:24:04 +0300

    JS/script.js

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+function tap() {
+var functiona = document.getElementsById("data1").value;
+var functionb = document.getElementsById("data2").value;
+var functionc = document.getElementsById("data3").value;
+
+
+else if((a + b) <=c || (b + c) <=a || (a + c) <=b) {
+  alert("not a triangle");
+}
+else if(a===b && b===c) {
+  alert("this is an equilateral");
+}
+else if(a===b || b===c || a===c) {
+  alert("this is an isosceles triangle");
+}else{
+  alert(scalene);
+}

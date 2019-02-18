6d8d5fc68c2a4f09bd787156520d811b90070ef6
Leah Gakii <leahgakii@gmail.com>
2019-02-18 09:51:45 +0300

    JS/script.js

--- JS/script.js
+++ JS/script.js
@@ -1,19 +1,13 @@
-var triangleTracker = function() {
-var a = parseFloat(prompt("enter the length"));
-var b = parseFloat(prompt("enter the height"));
-var c = parseFloat(prompt("enter the hypo"));
-if(isNaN(a)===true || isNaN(b)===true || isNaN(c)===true || a<=0 || b<=0 || c<=0) {
-  alert("enter a correct number")
-}
-else if((a + b) <=c || (b + c) <=a || (a + c) <=b) {
-  alert("not a triangle");
-}
-else if(a===b && b===c) {
-  alert("this is an equilateral");
-}
-else if(a===b || b===c || a===c) {
-  alert("this is an isosceles triangle");
-}else{
-  alert(scalene);
-}
+function triangleTracker()  {
+   var a = parseInt(document.getElementById('hypotenuse').value);
+   var b = parseInt(document.getElementById('length').value);
+   var c = parseInt(document.getElementById('height').value);
+
+   if ((a === b) && (b === c) && (a === b)) {
+       alert("equilateral triangle")
+   } else if (a === b || b === c || a === c) {
+       alert("isosceles triangle");
+   } else if (a != b && b != c && a != c ) {
+       alert("scalene triangle");
+  }
 }
